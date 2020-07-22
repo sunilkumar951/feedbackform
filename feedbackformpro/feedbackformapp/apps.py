d5fe73e88c4a3332b51416b414bd62086c2216f8
sunil <sunilyerramsetti15@gmail.com>
2020-07-22 12:30:00 +0530

    firstcommit

--- feedbackformpro/feedbackformapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FeedbackformappConfig(AppConfig):
-    name = 'feedbackformapp'
--- feedbackformpro/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!F:\Django-7am-Batch\feedbackformpro\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
